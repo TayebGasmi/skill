c1329283585c83a6f25b1989e9eb751c7e9a80ca
tayeb <tayeb.gasmi@esprit.tn>
2023-08-14 22:57:11 +0100

    adding @Serial

--- src/main/java/com/example/skill/entity/BaseEntity.java
+++ src/main/java/com/example/skill/entity/BaseEntity.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @MappedSuperclass
@@ -21,6 +22,7 @@ import java.io.Serializable;
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 @SuperBuilder(toBuilder = true)
 public class BaseEntity implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
